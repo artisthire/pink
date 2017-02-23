6991a7d227a0eee37d6c59dc241878f49d4f1e82
artisthire <artist_hire@mail.ru>
2017-02-23 15:44:13 +0200

    оформил и анимировал меню

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var toggle_btn = document.querySelector('.js-toggle-btn');
+var toggle_btn_SVG = document.querySelector('.js-toggle-btn__icon>use');
+
+var menu_wrap = document.querySelector('.js-page-header__nav-wrap');
+var menu = document.querySelector('.js-main-nav');
+
+toggle_btn.onclick = function(e) {
+   
+   var btn_active = this.classList.contains("menu-toggle-btn--active");
+
+   if (btn_active) {
+     toggle_btn_SVG.setAttribute("xlink:href", "#menu-burger");
+     this.classList.remove("menu-toggle-btn--active");
+     menu_wrap.classList.remove("page-header__nav-wrap--menu-open");
+     menu.classList.add("main-nav--hide");
+   } else {
+     toggle_btn_SVG.setAttribute("xlink:href", "#menu-cross");
+     this.classList.add("menu-toggle-btn--active");
+     menu_wrap.classList.add("page-header__nav-wrap--menu-open");
+     menu.classList.remove("main-nav--hide");
+   }
+   
+   
+
+ }
